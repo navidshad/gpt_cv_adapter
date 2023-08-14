06e735551bad4daccf45eaa60c8e7be9f7864a9a
Navid Shad <navidshad72@gmail.com>
2023-08-14 19:19:49 +0300

    Add PDF generator

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ template_chain = get_template_chain(openai_api_key)
 cover_letter_chain = get_cover_letter_chain(openai_api_key)
 
 # Read CV & Job Description from file
-jobs_dir = ".jobs"
-cv_adapted_dir = ".cv_adapted"
-cv_content = read_file_content("cv.txt")
+jobs_dir = "data/jobs"
+cv_adapted_dir = "data/cv_adapted"
+cv_content = read_file_content("data/cv.txt")
 html_templates = read_file_content("templates/tailwind_01.html")
 jobs = get_files_list(jobs_dir, ".txt")
 total_jobs = len(jobs)
@@ -33,10 +33,10 @@ for job in jobs:
     job_title = job.split(".")[0]
     md_file_name = f"{cv_adapted_dir}/{job_title}.md"
     html_file_name = f"{cv_adapted_dir}/{job_title}.html"
+    pdf_file_name = f"{cv_adapted_dir}/{job_title}.pdf"
     coverletter_file_name = f"{cv_adapted_dir}/{job_title} _cover.txt"
     job_description = read_file_content(f"{jobs_dir}/{job}")
 
-    # continue if file is already exist
     if not is_file_exist(md_file_name):
         print(f"{counter}/{total_jobs} Running CV adapter for {job_title}")
         result = cv_chain.run(
@@ -70,6 +70,16 @@ for job in jobs:
             }
         )
         write_file_content(coverletter_file_name, result)
+
+    if not is_file_exist(pdf_file_name):
+        print(f"{counter}/{total_jobs} Running PDF converter for {job_title}")
+
+        # find script path
+        script_path = os.path.dirname(os.path.realpath(__file__))
+
+        # rum nodejs script to convert html to pdf, put full HTML_FILE_PATH as env variable for nodejs script
+        os.environ["HTML_FILE_PATH"] = os.path.join(script_path, html_file_name)
+        os.system("node pdf_generator.js")
 
     counter += 1
 
---
+++ pdf_generator.js
@@ -0,0 +1,49 @@
+const puppeteer = require('puppeteer-core');
+const fs = require('fs');
+const path = require('path');
+
+const generatePDF = async () => {
+
+	const html_file_path = path.join(process.env.HTML_FILE_PATH) //process.env.HTML_FILE_PATH;
+
+	const browser = await puppeteer.launch({
+		headless: true,
+		executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+		args: ['--no-sandbox', '--disable-setuid-sandbox']
+	});
+
+	const page = await browser.newPage();
+	await page.goto(`file://${html_file_path}`, {
+		waitUntil: 'networkidle0'
+	});
+
+	await page.setViewport({
+		width: 1280,
+		height: 1024
+	});
+
+	const container = await page.$('#cv-container');
+
+	// get hight of body
+	const bodyHeight = await container.evaluate((_container) => {
+		return _container.clientHeight;
+	});
+
+	const pdf = await page.pdf({
+		width: 1280 / 96 + 'in',
+		height: ((bodyHeight / 96) - 4.5) + 'in',
+	});
+
+	await browser.close();
+
+	const pdf_file_path = html_file_path.replace('.html', '.pdf');
+
+	// Save pdf file
+	fs.writeFile(pdf_file_path, pdf, (err) => {
+		if (err) {
+			console.log(err);
+		}
+	});
+}
+
+generatePDF();
