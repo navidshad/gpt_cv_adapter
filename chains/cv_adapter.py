05f9355992d5c3f44a7c62429df17ab4f2b4b990
Navid Shad <navidshad72@gmail.com>
2023-08-15 01:06:25 +0300

    improve cv prompt

--- chains/cv_adapter.py
+++ chains/cv_adapter.py
@@ -24,12 +24,14 @@ This is the job description:
 {job_description}
 ```
 
-So perform the following steps:
-1. Change the summary of the CV to match the job description,
-2. Then replace the new summary to the CV content and create a new CV in markdown format.
-3. Generate a cover letter for the job description based on the CV content with maximom 2 paragraphs.
-4. then return only a JSON object like this {json_object} and nothing else.
-5. you have to return only a JSON object like this {json_object} and nothing else.
+Perform the following steps:
+1. Change the summary of the CV and match it with the job description.
+2. Find a proper [Position Title] for the CV based on the job description.
+3. Then replace the new summary to the CV content and create a new CV in markdown format.
+4. Remove each [Other skills] item of CV cntent if not mentione in the job description.
+5. Generate a cover letter for the job description based on the CV content with maximom 2 paragraphs. if ther is skills in the job description that are not in the CV, list them to into cover letter and say I will learn them.
+6. then return only a JSON object like this {json_object} and nothing else.
+7. you have to return only a JSON object like this {json_object} and nothing else.
 """
 
 human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
